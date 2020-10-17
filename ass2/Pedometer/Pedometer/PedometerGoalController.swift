e9f64374062d3a60771eaf3c1fbb13aa81d4e076
Jack Hu <jingxujaster@hotmail.com>
2020-10-17 03:41:05 -0400

    Here is the updated UI our next step is to figure out passing data between view controllers and the graph profile thing

--- ass2/Pedometer/Pedometer/PedometerGoalController.swift
+++ ass2/Pedometer/Pedometer/PedometerGoalController.swift
@@ -5,6 +5,7 @@
 //  Created by Jack Hu on 2020-10-13.
 //
 
+// imports
 import Foundation
 import UIKit
 
@@ -12,15 +13,49 @@ import UIKit
 // In this class, the controller will focus on implementing services/call backs to
 // Check when the goal for the number of steps have been reached or not
 // And user can input their own step goals, then application persists the data 
-class PedometerGoalController: UIViewController {
+class PedometerGoalController: UIViewController, UITextFieldDelegate {
 
+    // Outlets
+    // This is user input field to get desired goal data
+    @IBOutlet weak var userGoalInput: UITextField!
+    
+    // Local variables for storing the user inputed text field
+    var inputText = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set how the textfield repond to user input actions using UITextFiled protocols
+        // So class (UI view controller) has to implement the UITextFieldDelegate functions
+        self.userGoalInput.delegate = self
+        // Allows dismissing keyboard when tapping anywhere on the view controller
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated);
+    
+    // Actions
+    // This button when called will get the user inputed data from the UITextField
+    @IBAction func setMyGoalButton(_ sender: UIButton){
+        self.inputText = self.userGoalInput.text!
+        self.userGoalInput.text = ""
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! PedometerMainController
+        vc.userInputFromTextField = self.inputText
+    }
+    
+    func hideKeyBoard() {
+        self.userGoalInput.resignFirstResponder()
+    }
+    
+    // UITextFieldDelegate methods - they are optional to implement but I will only implement one of them in order to make keyboard disappear upon touch return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // textfiled relinquish first responder status and returns to view controller and hides keyboard when return button is pressed
+        hideKeyBoard()
+        return true
+    }
+    
 }
+
+
--- ass2/Pedometer/Pedometer/PedometerMainController.swift
+++ ass2/Pedometer/Pedometer/PedometerMainController.swift
@@ -7,59 +7,120 @@
 
 import UIKit
 import CoreMotion
-
 // This class will represent our main pedometer UI view controller upon loading up the app
 // In here we will able to see the live step count the user would like to record, progress bar, etc
 class PedometerMainController: UIViewController {
-    let activityManaging = CMMotionActivityManager()
-    let pedoMeter = CMPedometer()
-    let appQueue = OperationQueue()
-    let motionerror = NSError()
+    
+    // Local variable to store user input
+    var userInputFromTextField = ""
+    
+    // Connecting the labels and progress bar
+    @IBOutlet weak var activityType: UILabel!
+    @IBOutlet weak var distanceMeasure: UILabel!
+    @IBOutlet weak var stepCount: UILabel!
+    @IBOutlet weak var myGoal: UILabel!
+    @IBOutlet weak var myProgressBar: UIProgressView!
+    
+    // Instantiating the acitivity manager to determine sitting or walking etc and pedometer instances
+    // For gathering live and old saved data
+    let myActivityManager = CMMotionActivityManager()
+    let myPedometer = CMPedometer()
+    
+    
     // Adding the goal button so we could switch views and go the goal view
-    @IBAction func goalButtonPress(_ sender: UIButton) {
+    @IBAction func goalViewButtonPress(_ sender: UIButton) {
         performSegue(withIdentifier: "Goal", sender: self)
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = UIColor.systemPurple
         self.navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        startWalking()
-
-        
-    }
-    func startWalking () -> Void {
-        if (CMPedometer.isStepCountingAvailable() && CMPedometer.authorizationStatus() == .authorized){
-            CMPedometer.shared.startUpdates(from: Date.init()) { (stepData, error) in
-                if let stepcount = stepData?.numberOfSteps{
-                    //TODO: Show on graph
-                }
-            
-//                if let stepcount = dataf?.
-            
-            //CMPedometer.shared.startEventUpdates(handler: motionerror) in
-            }
-        }else if (CMPedometer.authorizationStatus() != (.authorized)){
-            //TODO: Send User message telling them to authorize
+        // Setting my goal and step count labels to a certain value on start up
+        self.stepCount.text = String(0)
+        // Setting my progress bar to 0 at start up
+        self.myProgressBar.progress = 0.0
+        // Set my default acitivity to null
+        self.activityType.text = ""
+        // Set distance traveled on start up to 0
+        self.distanceMeasure.text = String(0)
+        // Setting the goal = to the user inputed goal from the other view controller or
+        // 0 from initial startup 
+        if (self.userInputFromTextField == "") {
+            self.myGoal.text = String(0)
+        } else {
+            self.myGoal.text = self.userInputFromTextField
         }
         
+        
+        // Setting up the time and date for pedometer data extraction
+        let calender = NSCalendar.current
+        var calComponents = calender.dateComponents(in: TimeZone.current, from: Date())
+        calComponents.hour = 0
+        calComponents.minute = 0
+        calComponents.second = 0
+        let midnightOfToday = calender.date(from: calComponents)!
+        
+        
+        // Starting my step counting activity and getting my current type of activity
+        if(CMMotionActivityManager.isActivityAvailable()) {
+            // Listener of my acitivity, it will keep give me updates of my actiivity which is store in data
+            self.myActivityManager.startActivityUpdates(to: OperationQueue.main) { (data) in
+                // Starts the listening of our data
+                DispatchQueue.main.async {
+                    if let myActivity = data {
+                        // Switching between the differnt types of activity
+                        if (myActivity.running == true) {
+                            print("User is running!")
+                            self.activityType.text = "Running"
+                        } else if(myActivity.stationary == true) {
+                            print("User is stationary!")
+                            self.activityType.text = "Stationary"
+                            
+                        } else if(myActivity.walking == true) {
+                            print("User is walking!")
+                            self.activityType.text = "Walking"
+                            
+                        } else if(myActivity.automotive == true) {
+                            print("User is in a car!")
+                            self.activityType.text = "Automotive"
+                            
+                        } else if(myActivity.cycling == true) {
+                            print("User is biking!")
+                            self.activityType.text = "Cycling"
+                        }
+                    }
+                }
+            }
+        }
+        
+        // If my pedometer has step counting and distance measure hardware support
+        // then count and measure the number of steps and distance traveled
+        // This does live updates
+        if (CMPedometer.isStepCountingAvailable() && CMPedometer.isDistanceAvailable()) {
+            self.myPedometer.startUpdates(from: Date()) { (pedometerData, error) in
+                // if we have no error occuring - running in safe mode
+                if (error == nil) {
+                    if let response = pedometerData {
+                        // starts the listening of the number of steps taken
+                        DispatchQueue.main.async {
+                            // updating the progress bar
+                            let sc = self.stepCount.text
+                            let scValue = Int(sc!)
+                            let mg = self.myGoal.text
+                            let mgValue = Int(mg!)
+                            self.myProgressBar.progress = Float((Float(scValue!)/Float(mgValue!)))
+                            print("My progress: \(self.myProgressBar.progress)\n")
+                            print("Number of Steps: \(response.numberOfSteps)\n")
+                            self.stepCount.text = "\(response.numberOfSteps)"
+                            print("Distance traveled: \(response.distance!.intValue)")
+                            self.distanceMeasure.text = "\(response.distance!.intValue)"
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
-    
-
-
 }
-
-// MARK: Code that we could incorporate
-//        if CMMotionManager.shared.isAccelerometerAvailable{
-//
-//        }
-//        if CMMotionActivityManager.isActivityAvailable(){
-//            CMMotionActivityManager.shared.startActivityUpdates(to: OperationQueue.main){ (data) in
-//                DispatchQueue.main
-//            }
-//        }

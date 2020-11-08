cc0d04e3142a62b0618b04835e311a6856ca6f5d
Jack Hu <jingxujaster@hotmail.com>
2020-11-08 02:40:05 -0500

    started the 3rd assignmnt

---
+++ ass3/SoC_Thunderboard_Info_App/SoC_Thunderboard_Info_App/ThunderboardDataVC.swift
@@ -0,0 +1,20 @@
+//
+//  ThunderboardDataVC.swift
+//  SoC_Thunderboard_Info_App
+//
+//  Created by Jack Hu on 2020-11-07.
+//
+
+import UIKit
+
+class ThunderboardDataVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
---
+++ ass3/SoC_Thunderboard_Info_App/SoC_Thunderboard_Info_App/ThunderboardMainVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SoC_Thunderboard_Info_App
+//
+//  Created by Jack Hu on 2020-11-07.
+//
+
+import UIKit
+
+class ThunderboardMainVC: UIViewController {
+    
+    // outlets
+    
+    
+    
+    
+    // performs segue
+    @IBAction func viewData(_ sender: UIButton) {
+        performSegue(withIdentifier: "GetData", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

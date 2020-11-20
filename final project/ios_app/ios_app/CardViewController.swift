4ced1bbc3446fd596098706bbc0d03188be9891e
haaaamza <mian.hamza@mail.mcgill.ca>
2020-11-21 00:07:13 +0300

    Initial UI Commit
    Basic UI with Slide Up-Popover (taken from Github)

---
+++ final project/ios_app/ios_app/CardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CardViewController.swift
+//  ios_app
+//
+//  Created by Hamza Mian on 2020-11-20.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    
+    @IBOutlet weak var handleView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

c88aac4ef6508fe61801c6276efb82117990c842
jhu960213 <jingxujaster@hotmail.com>
2020-12-01 23:14:58 -0500

    added beacon code

---
+++ final project/soc_ibeacon/GNU ARM v7.2.1 - Debug/gecko_sdk_3.0.0/platform/service/system/src/sl_system_process_action.d
@@ -0,0 +1,8 @@
+gecko_sdk_3.0.0/platform/service/system/src/sl_system_process_action.o: \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.0/platform/service/system/src/sl_system_process_action.c \
+ /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.0//platform/common/toolchain/inc/sl_gcc_preinclude.h \
+ /Users/jhu69/SimplicityStudio/v5_workspace/soc_ibeacon/autogen/sl_event_handler.h
+
+/Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.0//platform/common/toolchain/inc/sl_gcc_preinclude.h:
+
+/Users/jhu69/SimplicityStudio/v5_workspace/soc_ibeacon/autogen/sl_event_handler.h:
---
+++ final project/soc_ibeacon/GNU ARM v7.2.1 - Debug/sources.mk
@@ -0,0 +1,40 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+autogen \
+gecko_sdk_3.0.0/platform/Device/SiliconLabs/EFR32BG22/Source/GCC \
+gecko_sdk_3.0.0/platform/Device/SiliconLabs/EFR32BG22/Source \
+gecko_sdk_3.0.0/platform/bootloader/api \
+gecko_sdk_3.0.0/platform/common/src \
+gecko_sdk_3.0.0/platform/common/toolchain/src \
+gecko_sdk_3.0.0/platform/emdrv/nvm3/src \
+gecko_sdk_3.0.0/platform/emlib/src \
+gecko_sdk_3.0.0/platform/radio/rail_lib/plugin/pa-conversions \
+gecko_sdk_3.0.0/platform/service/device_init/src \
+gecko_sdk_3.0.0/platform/service/mpu/src \
+gecko_sdk_3.0.0/platform/service/power_manager/src \
+gecko_sdk_3.0.0/platform/service/sleeptimer/src \
+gecko_sdk_3.0.0/platform/service/system/src \
+gecko_sdk_3.0.0/protocol/bluetooth/src \
+gecko_sdk_3.0.0/util/silicon_labs/silabs_core/memory_manager \
+gecko_sdk_3.0.0/util/third_party/crypto/sl_component/se_manager/src \
+gecko_sdk_3.0.0/util/third_party/mbedtls/library \
+gecko_sdk_3.0.0/util/third_party/mbedtls/sl_crypto/src/cryptoacc/src \
+gecko_sdk_3.0.0/util/third_party/mbedtls/sl_crypto/src \
+
